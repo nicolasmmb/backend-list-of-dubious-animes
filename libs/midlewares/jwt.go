a2f8c06f5604d97eaf7613309f0bcf14032236ed
nicolas.mmb <nicolas.mmb@hotmail.com>
2024-05-05 21:42:23 -0300

    refactor: Add JWT validation middleware

---
+++ libs/midlewares/jwt.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JwtValidate(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// t := opentel.GetTracer()
+		// _, span := t.Start(c.Request.Context(), "route.validate-credentials")
+		// defer span.End()
+		token := c.GetHeader("Authorization")
+		if token == "" {
+			c.JSON(401, gin.H{"error": "Token is required"})
+			c.Abort()
+			return
+		}
+		token = strings.Replace(token, "Bearer ", "", 1)
+
+		claims := jwt.MapClaims{}
+		_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		})
+		if err != nil {
+			c.JSON(401, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("jwt", claims)
+		c.Next()
+	}
+}
